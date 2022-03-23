88d68928c14c8baa3604bfc01eb368a37862533a
Kasia Laniecka <kl@parkshare.dk>
2022-03-23 17:51:53 +0100

    errors

--- lesson-08/remix-mongoose/app/db/models.js
+++ lesson-08/remix-mongoose/app/db/models.js
@@ -4,6 +4,24 @@ const { Schema } = mongoose;
 
 const bookSchema = new Schema({
   title: String,
+  author: String,
+  price: Number,
+  currency: String,
+  isbn13: String,
+  description: String,
+  starRating: Number,
+  ratingsCount: Number,
+  language: String,
+  publicationDate: Date,
+  publisher: String,
+  categories: Array,
+  reviews: [
+    {
+      author: String,
+      description: String,
+      starRating: Number,
+    },
+  ],
 });
 
 export const models = [
--- lesson-08/remix-mongoose/app/routes/books/$bookId.jsx
+++ lesson-08/remix-mongoose/app/routes/books/$bookId.jsx
@@ -1,19 +1,56 @@
-import { useLoaderData } from "remix";
+import { useLoaderData, useCatch, json } from "remix";
 import connectDb from "~/db/connectDb.server.js";
 
 export async function loader({ params }) {
   const db = await connectDb();
-  return db.models.Book.findById(params.bookId);
+  const book = await db.models.Book.findById(params.bookId);
+
+  if (!book) {
+    throw new Response(`Couldn't find book with ID ${params.bookId}`, {
+      status: 404,
+    });
+  }
+
+  return json(book);
 }
 
 export default function BookPage() {
   const book = useLoaderData();
+
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">{book.title}</h1>
-      <code>
-        <pre>{JSON.stringify(book, null, 2)}</pre>
-      </code>
+      <p>Author: {book.author}</p>
+      <br></br>
+      <p>
+        Price: {book.price} {book.currency}
+      </p>
+      <br></br>
+      <p>{book.description}</p>
+      {/* {book.reviews.map((review) => {
+        return <p>{review.description}</p>;
+      })} */}
     </div>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">
+        {caught.status} {caught.statusText}
+      </h1>
+      <p>{caught.data}</p>
+    </div>
+  );
+}
+
+export function ErrorBoundary({ error }) {
+  return (
+    <h1 className="text-2xl font-bold mb-4">
+      {error.name}: {error.message}
+    </h1>
   );
 }
